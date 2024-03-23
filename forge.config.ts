3c462cd269c1ad57c3f6a86b6d56b0d2d9f694d5
Lukas Bach <contact@lukasbach.com>
2024-03-23 17:22:41 +0100

    react setup

--- forge.config.ts
+++ forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    extraResource: "./extra"
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
---
+++ src/renderer.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <div>Hello</div>,
+);
