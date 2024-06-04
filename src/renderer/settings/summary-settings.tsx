92356a424a76d095df7efc93583c91d665c21bcf
Lukas Bach <contact@lukasbach.com>
2024-06-05 01:42:18 +0200

    add gpt 4o

--- src/renderer/settings/summary-settings.tsx
+++ src/renderer/settings/summary-settings.tsx
@@ -29,8 +29,8 @@ export const OpenAiSettings: FC = () => {
         field="llm.providerConfig.openai.chatModel.model"
         form={form}
         values={[
+          "gpt-4o",
           "gpt-4-turbo",
-          "gpt-4-turbo-2024-04-09",
           "gpt-4-turbo-preview",
           "gpt-4-0125-preview",
           "gpt-4-1106-preview",
