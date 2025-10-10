c5723f3a687ac15ce28f1dc3d62ceb86ea6fe6e6
Lukas Bach <contact@lukasbach.com>
2025-10-10 11:54:40 +0200

    feat: macos deploy pipeline

--- forge.config.ts
+++ forge.config.ts
@@ -3,6 +3,7 @@ import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 // import { MakerAppX } from "@electron-forge/maker-appx";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
@@ -47,6 +48,9 @@ const config: ForgeConfig = {
     new MakerZIP({}, ["darwin"]),
     new MakerRpm({}),
     new MakerDeb({}),
+    new MakerDMG({
+      icon: "./extra/icon@8x.ico",
+    }),
   ],
   plugins: [
     new VitePlugin({
@@ -93,7 +97,7 @@ const config: ForgeConfig = {
           name: "pensieve",
         },
         prerelease: false,
-        draft: false,
+        draft: true,
         generateReleaseNotes: true,
       },
     },
